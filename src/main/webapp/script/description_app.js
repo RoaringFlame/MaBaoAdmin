f8d8ad07756f2b89b2f0a0c8ccb95a562f0bef9b
Emma <1245095397@qq.com>
2016-08-15 11:50:59 +0800

    图片！

--- src/main/webapp/script/description_app.js
+++ src/main/webapp/script/description_app.js
@@ -1,3 +1,21 @@
 /**
  * Created by Emma on 2016/8/15.
+ * 轮播时间设置
  */
+"use strict";
+$(function () {
+    //设定轮播时间2秒
+    function initCarousel() {
+        $(function () {
+            $('.carousel').carousel({
+                interval: 2500
+            })
+        });
+    }
+
+    function init() {
+        initCarousel();
+    }
+
+    init();
+});
