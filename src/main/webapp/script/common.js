fa1dc861ac4628fb928ad50d3d488122d298b845
zhangqian <1139384196@qq.com>
2016-08-17 12:04:36 +0800

    增加管理员角色修改

--- src/main/webapp/script/common.js
+++ src/main/webapp/script/common.js
@@ -4,7 +4,7 @@
  */
 "use strict";
 $(function () {
-    function initPage(){
+    function initPage() {
         var arr = [
             ['商品列表', 'goods_list_managed'],
             ['商品分类', 'goods_type_managed'],
@@ -12,7 +12,7 @@ $(function () {
             ['发货单', 'invoices_managed'],
             ['转让订单', 'transfer_order_managed'],
             ['账户管理', 'user_managed'],
-            ['查看日志', 'log_managed'],
+            ['查看日志', 'log_managed']
         ];
 
 
@@ -27,8 +27,8 @@ $(function () {
             })
         });
 
-        $("#goods_manage").find("a").click(function(){
-            $("#nav_change").find("li:eq(0)>ul>li:eq(2)>a").removeClass("show");
+        $("#goods_manage").find("a").click(function () {
+            $("#nav_change").removeClass("hide");
             $("#nav_change").find("li:eq(0)>a").text("商品管理");
             $("#nav_change").find("li:eq(0)>ul>li:eq(0)>a").text("商品列表");
             $("#nav_change").find("li:eq(0)>ul>li:eq(0)>a").click(function () {
@@ -37,7 +37,7 @@ $(function () {
                 })
             });
 
-            $("#nav_change").find("li:eq(0)>ul>li:eq(1)>a").text("商品分类");
+            $("#nav_change").find("li:eq(0)>a").text("商品分类");
             $("#nav_change").find("li:eq(0)>ul>li:eq(1)>a").click(function () {
                 $('iframe').attr('src', function () {
                     return arr[1][1] + '.html';
@@ -45,7 +45,8 @@ $(function () {
             });
         });
 
-        $("#order_manage").find("a").click(function(){
+        $("#order_manage").find("a").click(function () {
+            $("#nav_change").removeClass("hide");
             $("#nav_change").find("li:eq(0)>a").text("订单管理");
             $("#nav_change").find("li:eq(0)>ul>li:eq(0)>a").text("订单");
             $("#nav_change").find("li:eq(0)>ul>li:eq(0)>a").click(function () {
@@ -61,7 +62,7 @@ $(function () {
                 })
             });
 
-            $("#nav_change").find("li:eq(0)>ul>li:eq(2)>a").addClass("show");
+
             $("#nav_change").find("li:eq(0)>ul>li:eq(2)>a").click(function () {
                 $('iframe').attr('src', function () {
                     return arr[4][1] + '.html';
@@ -70,8 +71,8 @@ $(function () {
 
         });
 
-        $("#user_manage").find("a").click(function(){
-            $("#nav_change").find("li:eq(0)>ul>li:eq(2)>a").removeClass("show");
+        $("#user_manage").find("a").click(function () {
+            $("#nav_change").removeClass("hide");
             $("#nav_change").find("li:eq(0)>a").text("用户管理");
             $("#nav_change").find("li:eq(0)>ul>li:eq(0)>a").text("账户管理");
             $("#nav_change").find("li:eq(0)>ul>li:eq(0)>a").click(function () {
@@ -87,10 +88,23 @@ $(function () {
                 })
             });
         });
+
+        $(".navbar-brand").click(function () {
+            $("#nav_change").addClass("hide");
+            $('iframe').attr('src', function () {
+                return  'index.html';
+            })
+        });
+
     }
 
-    function init(){
+
+    function init() {
+
         initPage();
+
     }
+
     init();
+
 });
